c890b207d95e25ed7a25641f93938e5bbec586fb
Abhishek Jathan <abhishekjathan321@gmail.com>
2021-08-11 17:22:27 +0530

    minor

--- assignment-4/assignment4/partB.java
+++ assignment-4/assignment4/partB.java
@@ -1,5 +1,11 @@
+/*
+ Develop a java applet that describes about yourself, your academic achievements
+and your School/Institute
+Note: The applet must contain at least 8 swing components.
+ */
+
 package assignment4;
 
 public class partB {
-
+	
 }
